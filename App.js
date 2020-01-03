e42cb78f3079f0e73b88d5811aa97a206d77148b
rahul830ui <rahul830ui@gmail.com>
2020-01-04 01:23:03 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+
+import HomeScreen from "./src/screens/HomeScreen";
+
+const navigator = createStackNavigator({
+    Home: HomeScreen
+}, {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+        title: 'Rice'
+    }
+});
+
+export default createAppContainer(navigator);
